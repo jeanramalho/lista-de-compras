d0ecac0eee4fadca3addbea83cd00a56e1f4ff5a
jeanramalho <jeanramalho.dev@gmail.com>
2022-12-01 13:04:31 -0300

    Cria container no Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import { Container } from "./style";
 
 function Header() {
-    return
+    return (
+        <Container>
+            
+
+        </Container>
+    )
 }
 
 export default Header
