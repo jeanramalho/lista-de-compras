0d492ef9ba05a5832458119429076ed1d74e63a0
jeanramalho <jeanramalho.dev@gmail.com>
2022-12-01 13:06:40 -0300

    Cria um título no header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,11 @@ import { Container } from "./style";
 function Header() {
     return (
         <Container>
+            <div>
+                <h1 className="titulo">Lista de Compras</h1>
+            </div>
             
+
 
         </Container>
     )
